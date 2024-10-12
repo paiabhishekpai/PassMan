95847860b82f28a3b61e262eebd206cf78b80a67
paiabhishekpai <abhishekupai1124@gmail.com>
2024-10-13 00:56:54 +0530

    adding a background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Manager from './components/Manager'
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Manager/>
       <div>my UI</div>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-slate-400 flex justify-between p-2'>
+    <nav className='bg-black text-white flex justify-between p-2'>
         <div className="logo px-4 font-bold ">PassMan</div>
         <ul className='flex gap-5 justify-between px-3'>
             <li className='cursor-pointer hover:font-bold'>Home</li>
