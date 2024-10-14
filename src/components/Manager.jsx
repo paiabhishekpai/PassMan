0129a467fb80e80e5b48ea531bd5c1e42f3bab87
paiabhishekpai <abhishekupai1124@gmail.com>
2024-10-14 21:37:05 +0530

    UI updated

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Manager = () => {
   return (
     <>
-   <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="mycontainer   max-w-4xl">
+      <h1 className='text-4xl text font-bold text-center'>
+                    <span className='text-green-500'> &lt;</span>
+                    <span className="text-white">Pass</span><span className='text-green-500'>Man/&gt;</span>
+                </h1>
+                <p className='text-green-700 text-lg text-center'>Manage all your passwords at one place!</p>
+        <div className="flex flex-col p-4 text-white">
+          <input className="rounded-full border border-green-800 w-full p-4 py-1" type="text" />
+        </div>
+        <div className="flex gap-2 w-full">
+        <input className="rounded-full border border-green-800 w-full p-4 py-1" type="text" />
+        <input className="rounded-full border border-green-800 w-full p-4 py-1" type="text" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Manager
+export default Manager;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-black text-white flex justify-between p-2'>
-        <div className="logo px-4 font-bold ">PassMan</div>
+    <nav className='bg-black text-white '>
+      <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+        <div className="logo px-4 font-bold ">
+        <span className='text-green-500'> &lt;</span>
+        <span>Pass</span><span className='text-green-500'>Man/&gt;</span>
+        </div>
         <ul className='flex gap-5 justify-between px-3'>
             <li className='cursor-pointer hover:font-bold'>Home</li>
             <li className='cursor-pointer hover:font-bold'>About</li>
             <li className='cursor-pointer hover:font-bold'>Contact</li>
         </ul>
+      </div>
     </nav>
   )
 }
