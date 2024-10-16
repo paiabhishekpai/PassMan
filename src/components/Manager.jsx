b4b685c274df9af594ce458899b0fe94b00abbb7
paiabhishekpai <abhishekupai1124@gmail.com>
2024-10-16 19:35:13 +0530

    icons for show and hide password

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -26,11 +26,17 @@ const Manager = () => {
               type="text"
               placeholder="Enter username"
             />
+            <div className="relative">
             <input
               className="rounded-full border border-green-800 w-full p-4 py-1"
               type="text"
               placeholder="Enter password"
             />
+            <span className="absolute right-1 top-1.5"> 
+              <img width={20} src="icons/eye.png" alt="eye" />
+            </span>
+            </div>
+            
           </div>
           <button className=" flex mx-auto my-2 gap-1 justify-center items-center bg-green-400 text-black font-bold hover:font-extrabold hover:bg-green-500 px-3 p-1 rounded-full">
             <lord-icon
